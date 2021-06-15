7c00d692f99092704770ddae67069a8367f13e02
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-06-15 08:58:49 -0700

    initial commit

---
+++ IterationStatementsLoopsExercise1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace IterationStatementsLoopsExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0, i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        
+            for (int i = 0, i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            
+            for (int i = 0, i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+
+            }
+            
+            for (int i = object, i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            
+            for (int i = 0, i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        
+            for (int i = , i < 5; i++)
+            { 
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = , i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = , i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = , i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = , i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = , i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = , i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = , i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = , i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+
+
+
+    }
+}
